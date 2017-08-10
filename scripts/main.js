d4aea0fc9689af8c369a4a9ba1625fc7284a2806
Spencer Schack <scs1818@gmail.com>
2017-08-09 19:10:51 -0700

    mobile glasses

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,33 @@
-window.addEventListener('mousemove', function(event) {
-  const glasses = document.querySelector('.glasses');
-  glasses.dataset.column = quantile(event.pageX / window.innerWidth);
-  glasses.dataset.row = quantile(event.pageY / window.innerHeight);
+window.addEventListener('DOMContentLoaded', function() {
+  if('ontouchstart' in window) {
+    window.addEventListener('deviceorientation', function(event) {
+      var x = 1 - clamp((event.gamma + 45) / 90);
+      var y = event.beta;
+      if(Math.abs(y) > 90) {
+        y = 1 - (Math.abs(y) - 90) / 90;
+      } else {
+        y /= 90;
+      }
+      y = 1 - clamp(y);
+      look(x, y);
+    });
+  } else {
+    window.addEventListener('mousemove', function(event) {
+      look(event.pageX / window.innerWidth, event.pageY / window.innerHeight);
+    });
+  }
+  var dataset = document.querySelector('.glasses').dataset;
+  function log() {
+    document.getElementById('log').innerHTML = Array.prototype.join.call(arguments, ',');
+  }
+  function look(x, y) {
+    dataset.column = quantile(x);
+    dataset.row = quantile(y);
+  }
+  function quantile(coordinate) {
+    return Math.min(4, Math.floor(coordinate * 5)) - 2;
+  }
+  function clamp(coordinate) {
+    return Math.max(Math.min(coordinate, 1), 0);
+  }
 });
-function quantile(x) {
-  return Math.max(Math.min(Math.floor(x * 5), 4), 0) - 2;
-}
